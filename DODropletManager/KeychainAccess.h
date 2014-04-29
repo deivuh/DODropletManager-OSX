9aa0b2380cd989eb3101c992b8027d15b4621f19
Daniel Parnell <me@danielparnell.com>
2014-04-29 17:06:49 +1000

    Ignore .DS_Store and xcuserdata

---
+++ DODropletManager/KeychainAccess.h
@@ -0,0 +1,13 @@
+//
+//  KeychainAccess.h
+//  DODropletManager
+//
+//  Created by Daniel Parnell on 29/04/2014.
+//  Copyright (c) 2014 David Hsieh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeychainAccess : NSObject
+
+@end
---
+++ DODropletManager/KeychainAccess.m
@@ -0,0 +1,13 @@
+//
+//  KeychainAccess.m
+//  DODropletManager
+//
+//  Created by Daniel Parnell on 29/04/2014.
+//  Copyright (c) 2014 David Hsieh. All rights reserved.
+//
+
+#import "KeychainAccess.h"
+
+@implementation KeychainAccess
+
+@end
