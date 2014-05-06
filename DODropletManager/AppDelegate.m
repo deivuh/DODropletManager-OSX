785bdbcdfddcb2607d94570a051e2a2ce1c91172
David Hsieh <davidhsiehlo@gmail.com>
2014-05-05 19:57:02 -0600

    Moved droplets to a singleton class

--- DODropletManager/AppDelegate.m
+++ DODropletManager/AppDelegate.m
@@ -10,35 +10,33 @@
 #import "Droplet.h"
 #import "KeychainAccess.h"
 #import "DropletFormWindowController.h"
+#import "DropletManager.h"
 
 
 @implementation AppDelegate {
-    NSMutableData *responseData;
-    NSMutableArray *dropletsArray;
-    
-    NSURLConnection *dropletsConnection, *regionsConnection, *imagesConnection;
-    
-    NSURLConnection *rebootDropletConnection, *shutdownDropletConnection, *turnOnDropletConnection;
     
     NSString *clientID, *APIKey;
     
-    NSMutableDictionary *regions;
-    NSMutableDictionary *images;
-    
+
     NSUserDefaults *userDefaults;
-    
     
     NSMenuItem *refreshMI;
     NSAlert *rebootAlert, *shutdownAlert;
     
     NSTimer *refreshingTimer;
+    
+    
     float fade, delta;
     
     BOOL firstRun;
+    
+    DropletManager *dropletManager;
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    dropletManager = [DropletManager sharedManager];
+    
     [self createMenuItems];
     
     if([self loadKeys]) {
@@ -46,6 +44,17 @@
     } else {
         [self showPreferencesWindow: self];
     }
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(receivedNotification:)
+                                                 name:@"dropletsLoaded"
+                                               object:nil];
+
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(receivedNotification:)
+                                                 name:@"dropletsFailed"
+                                               object:nil];
 
 }
 
@@ -54,178 +63,18 @@
     NSString *key;
     
     if([KeychainAccess getClientId: &client andAPIKey: &key error: nil]) {
-        clientID = client;
-        APIKey = key;
+        
+        dropletManager.clientID = client;
+        dropletManager.APIKey = key;
+        
+***REMOVED***        clientID = client;
+***REMOVED***        APIKey = key;
         
         return YES;
     }
     
     return NO;
 
-}
-
-#pragma mark -
-#pragma mark Communication methods
-
-- (void) requestRegions {
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https:***REMOVED***api.digitalocean.com/regions/?client_id=%@&api_key=%@", clientID, APIKey]];
-    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
-    regionsConnection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self];
-    
-    if(regionsConnection) {
-        responseData = [[NSMutableData alloc] init];
-    } else {
-        DLog(@"connection failed");
-    }
-
-}
-
-- (void) requestImages {
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https:***REMOVED***api.digitalocean.com/images/?client_id=%@&api_key=%@", clientID, APIKey]];
-    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
-    imagesConnection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self];
-    
-    if(imagesConnection) {
-        responseData = [[NSMutableData alloc] init];
-    } else {
-        DLog(@"connection failed");
-    }
-}
-
-- (void) requestDroplets {
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https:***REMOVED***api.digitalocean.com/droplets/?client_id=%@&api_key=%@", clientID, APIKey]];
-    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
-    dropletsConnection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self];
-    
-    if(dropletsConnection) {
-        responseData = [[NSMutableData alloc] init];
-    } else {
-        DLog(@"connection failed");
-    }
-    
-}
-
-- (void) requestRebootForDroplet:(Droplet*)droplet {
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https:***REMOVED***api.digitalocean.com/droplets/%@/reboot/?client_id=%@&api_key=%@", droplet.dropletID, clientID, APIKey]];
-    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
-    rebootDropletConnection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self];
-    
-    if(rebootDropletConnection) {
-        responseData = [[NSMutableData alloc] init];
-    } else {
-        DLog(@"connection failed");
-    }
-}
-
-- (void) requestShutdownForDroplet:(Droplet*)droplet {
-
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https:***REMOVED***api.digitalocean.com/droplets/%@/shutdown/?client_id=%@&api_key=%@", droplet.dropletID, clientID, APIKey]];
-    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
-    shutdownDropletConnection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self];
-    
-    if(shutdownDropletConnection) {
-        responseData = [[NSMutableData alloc] init];
-    } else {
-        DLog(@"connection failed");
-    }
-    
-}
-
-- (void) requestTurnOnForDroplet:(Droplet*)droplet {
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https:***REMOVED***api.digitalocean.com/droplets/%@/power_on/?client_id=%@&api_key=%@", droplet.dropletID, clientID, APIKey]];
-    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
-    turnOnDropletConnection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self];
-    
-    DLog(@"Request turnOn %@", urlRequest.URL);
-    
-    if(turnOnDropletConnection) {
-        responseData = [[NSMutableData alloc] init];
-    } else {
-        DLog(@"connection failed");
-    }
-}
-
-
-- (void) connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
-{
-    [responseData setLength:0];
-}
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
-{
-    [responseData appendData:data];
-}
-
-- (void) connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
-{
-    connection = nil;
-    responseData = nil;
-    
-    DLog(@"connection error");
-    refreshMI.title = NSLocalizedString(@"Refresh", @"Refresh");
-    refreshMI.action = @selector(refresh:);
-    
-    [self setStatusImage: @"DropletStatusIconFailed"];
-}
-
-- (void) connectionDidFinishLoading:(NSURLConnection *)connection
-{
-    NSError *error;
-    
-    NSDictionary* json = [NSJSONSerialization
-                          JSONObjectWithData:responseData
-                          options:NSUTF8StringEncoding
-                          error:&error];
-    
-    if(json != nil)
-    {
-        if (connection == dropletsConnection) {
-            NSArray *tempDropletsArray = [json objectForKey:@"droplets"];
-            dropletsArray = [[NSMutableArray alloc] init];
-
-            for (NSDictionary *dropletDictionary in tempDropletsArray) {
-                Droplet *droplet = [[Droplet alloc] initWithDictionary:dropletDictionary regions:regions andImages:images];
-                [dropletsArray addObject:droplet];
-            }
-
-            [self createMenuItems];
-            refreshMI.title = @"Refresh";
-            refreshMI.action = @selector(refresh:);
-        } else  if (connection == regionsConnection) {
-            NSArray *tempRegionsArray = [json objectForKey:@"regions"];
-            regions = [[NSMutableDictionary alloc] init];
-            
-            for (NSDictionary *region in tempRegionsArray) {
-                NSString *regionID = [region objectForKey:@"id"];
-                NSString *regionName = [region objectForKey:@"name"];
-                
-                [regions setObject:regionName forKey:regionID];
-            }
-            
-            [self requestImages];
-        } else  if (connection == imagesConnection) {
-            NSArray *tempImagesArray = [json objectForKey:@"images"];
-            images = [[NSMutableDictionary alloc] init];
-            
-            for (NSDictionary *image in tempImagesArray) {
-                NSString *imageID = [image objectForKey:@"id"];
-                NSString *distro = [image objectForKey:@"name"];
-                
-                [images setObject:distro forKey:imageID];
-            }
-            
-            [self requestDroplets];
-        } else  if (connection == rebootDropletConnection) {
-            DLog(@"Result status %@", [json objectForKey:@"status"]);
-            [self refresh:self];
-        } else  if (connection == shutdownDropletConnection) {
-            DLog(@"Result status %@", json);
-            [self refresh:self];
-        } else  if (connection == turnOnDropletConnection) {
-            DLog(@"Result status %@", json);
-            [self refresh:self];
-        }
-    }
 }
 
 #pragma mark -
@@ -265,7 +114,7 @@
     
     int dropletMIIndex = 0;
     
-    for (Droplet *droplet in dropletsArray) {
+    for (Droplet *droplet in [dropletManager droplets]) {
         [menu insertItemWithTitle: droplet.name action: nil keyEquivalent: @"" atIndex: dropletMIIndex];
 
         NSMenu *submenu = [[NSMenu alloc] init];
@@ -395,7 +244,7 @@
     
     refreshMI.title = NSLocalizedString(@"Refreshing...", @"Refreshing...");
     refreshMI.action = nil;
-    [self requestRegions];
+    [dropletManager refreshDroplets];
 }
 
 - (void)confirmReboot:(id)sender {
@@ -437,7 +286,7 @@
     
     Droplet *currentDroplet = (Droplet*)sender;
     
-    [self requestRebootForDroplet:currentDroplet];
+    [dropletManager rebootDroplet:currentDroplet];
     
 }
 
@@ -445,14 +294,14 @@
     
     Droplet *currentDroplet = (Droplet*)sender;
     
-    [self requestShutdownForDroplet:currentDroplet];
+    [dropletManager shutdownDroplet:currentDroplet];
 }
 
 - (void)turnOnDroplet:(id)sender {
     
     Droplet *currentDroplet = ((NSMenuItem*)sender).representedObject;
     
-    [self requestTurnOnForDroplet:currentDroplet];
+    [dropletManager turnOnDroplet:currentDroplet];
 }
 
 - (void)viewDropletOnBrowser:(id)sender {
@@ -511,6 +360,30 @@
     [img unlockFocus];
     
     _statusItem.image = img;
+}
+
+
+#pragma mark -
+#pragma mark Notification methods
+
+- (void) receivedNotification:(NSNotification *) notification
+{
+    
+    if ([[notification name] isEqualToString:@"dropletsLoaded"]) {
+
+        [self createMenuItems];
+        refreshMI.title = @"Refresh";
+        refreshMI.action = @selector(refresh:);
+        
+        
+    } else if ([[notification name] isEqualToString:@"dropletsFailed"]) {
+    
+        refreshMI.title = NSLocalizedString(@"Refresh", @"Refresh");
+        refreshMI.action = @selector(refresh:);
+    
+        [self setStatusImage: @"DropletStatusIconFailed"];
+        
+    }
 }
 
 @end
---
+++ DODropletManager/DropletManager.h
@@ -0,0 +1,42 @@
+***REMOVED***
+***REMOVED***  DropletManager.h
+***REMOVED***  DODropletManager
+***REMOVED***
+***REMOVED***  Created by David Hsieh on 5/4/14.
+***REMOVED***  Copyright (c) 2014 David Hsieh. All rights reserved.
+***REMOVED***
+
+#import <Foundation/Foundation.h>
+#import "Droplet.h"
+
+@interface DropletManager : NSObject <NSURLConnectionDataDelegate, NSURLConnectionDelegate> {
+
+    NSString *someProperty;
+    
+    NSMutableData *responseData;
+
+    
+    NSURLConnection *dropletsConnection, *regionsConnection, *imagesConnection;
+    
+    NSURLConnection *rebootDropletConnection, *shutdownDropletConnection, *turnOnDropletConnection;
+
+    NSMutableDictionary *regions;
+    NSMutableDictionary *images;
+    
+}
+
+
+@property (nonatomic, strong) NSString *clientID;
+@property (nonatomic, strong) NSString *APIKey;
+@property (nonatomic, strong) NSMutableArray *droplets;
+
++ (id)sharedManager;
+
+
+- (void)refreshDroplets;
+- (void)rebootDroplet:(Droplet*)droplet;
+- (void)shutdownDroplet:(Droplet*)droplet;
+- (void)turnOnDroplet:(Droplet*)droplet;
+
+
+@end
